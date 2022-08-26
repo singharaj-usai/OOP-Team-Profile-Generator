81e395b14a542490b952b6489195699ed27ddbf1
Singharaj Usai <djjd22938@gmail.com>
2022-08-25 23:11:03 -0400

    Employee.test.js Describe wip

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,12 @@
+const Employee = require('../lib/Employee')
+const worker = Employee('Jared', '1', 'Jared@yahoo.com')//name, id, email
+
+describe('Child', () => {
+describe('Initialization', () => {
+    it('should create an object with a name and age if provided valid arguments', () => {
+        expect(child.name).toEqual('Sarah')
+    })
+})
+})
+
+//Describe var reference in OOP > 01 Activities > 18-Stu_First-Mock
