5c7dc481727af1dd55d1d34ad9c9b803343bf850
Singharaj Usai <djjd22938@gmail.com>
2022-08-26 00:06:56 -0400

    start of index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+//Basically make something similar to the node.js assignment
+const inquirer = require('inquirer')
+
+const fs = require('fs')
+//const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const Employee = require('./lib/Employee')
+
+const Intern = require('./lib/Intern')
+
+const Manager = require('./lib/Manager')
+
+const Engineer = require('./lib/Engineer')
