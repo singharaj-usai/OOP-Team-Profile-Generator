56eccdace820ef53441ddf3727a9341ee81387a3
singharaj <djjd22938@gmail.com>
2022-11-09 17:28:55 -0500

    .then

--- index.js
+++ index.js
@@ -74,87 +74,107 @@ const addManager = () => {
 };
 
 const addEmployee = () => {
-  return inquirer.prompt([
-    {
-      type: "list",
-      name: "role",
-      message: "What is the employee's role?",
-      choices: ["Engineer", "Intern"],
-    },
-    {
-      type: "input",
-      name: "name",
-      message: "What is the name of the employee?",
-      validate: (nameInput) => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log("Please enter an employee's name!");
-          return false;
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role?",
+        choices: ["Engineer", "Intern"],
       },
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is the employee's ID?",
-      validate: (idInput) => {
-        if (idInput) {
-          return true;
-        } else {
-          console.log("Please enter employee's ID!");
-          return false;
-        }
-      },
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is the employee's Email?",
-      validate: (email) => {
-        //Regex email
-        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-        if (valid) {
-          return true;
-        } else {
-          console.log("Please enter the employee's Email!");
-          return false;
-        }
-      },
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "What is the employee's GitHub username?",
-      when: (input) => input.role === "Engineer",
-      validate: usernameInput => {
-         if (usernameInput) {
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the employee?",
+        validate: (nameInput) => {
+          if (nameInput) {
             return true;
-         } else {
+          } else {
+            console.log("Please enter an employee's name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the employee's ID?",
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log("Please enter employee's ID!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the employee's Email?",
+        validate: (email) => {
+          //Regex email
+          valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+          if (valid) {
+            return true;
+          } else {
+            console.log("Please enter the employee's Email!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the employee's GitHub username?",
+        when: (input) => input.role === "Engineer",
+        validate: (usernameInput) => {
+          if (usernameInput) {
+            return true;
+          } else {
             console.log("Please enter the employee's GitHub username!");
             return false;
-         }
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "school",
-      message: "What is the intern's school?",
-      when: (input) => input.role === "Intern",
-      validate: schoolInput => {
-         if (schoolInput) {
+      {
+        type: "input",
+        name: "school",
+        message: "What is the intern's school?",
+        when: (input) => input.role === "Intern",
+        validate: (schoolInput) => {
+          if (schoolInput) {
             return true;
-         } else {
+          } else {
             console.log("Please enter the intern's school name!");
             return false;
-         }
+          }
+        },
       },
-    },
-    {
-      type: "confirm",
-      name: "confirm",
-      message: "Do you want to add more members?",
-      default: false
-    }
-  ])
+      {
+        type: "confirm",
+        name: "confirm",
+        message: "Do you want to add more members?",
+        default: false,
+      },
+    ])
+    .then((employeeData) => {
+      let { name, id, email, role, github, school, confirm } = employeeData;
+      let employee;
+
+      if (role === "Engineer") {
+        employee = new Engineer(name, id, email, github);
+        console.log(employee);
+      } else if (role === "Intern") {
+        employee = new Intern(name, id, email, school);
+        console.log(employee);
+      }
+      team.push(employee);
+
+      if (confirm) {
+        return addEmployee(team);
+      } else {
+        return team;
+      }
+    });
 };
