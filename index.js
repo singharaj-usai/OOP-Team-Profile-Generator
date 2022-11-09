b5d7969df8b622f4f6fd5fcc70072cfefd15e6f7
singharaj <djjd22938@gmail.com>
2022-11-09 17:08:26 -0500

    format

--- index.js
+++ index.js
@@ -8,66 +8,67 @@ const Manager = require("./lib/Manager");
 const team = [];
 
 const addManager = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Who is the manager?",
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log("Please enter manager's name!");
-          return false;
-        }
-      }
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is the manager's ID?",
-      validate: idInput => {
-        if (idInput) {
-          return true;
-        } else {
-          console.log("Please enter manager's ID!");
-          return false;
-        }
-      }
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is the manager's Email?",
-      validate: email => {
-         //Regex email
-         valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
-         if (valid) {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Who is the manager?",
+        validate: (nameInput) => {
+          if (nameInput) {
             return true;
-         } else {
-            console.log("Please enter manager's Email!")
+          } else {
+            console.log("Please enter manager's name!");
             return false;
-         }
-      }
-    },
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "What is the manager's office number?",
-      validate: officeInput => {
-         if (officeInput) {
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the manager's ID?",
+        validate: (idInput) => {
+          if (idInput) {
             return true;
-         } else {
-            console.log("Please enter manager's office number!")
+          } else {
+            console.log("Please enter manager's ID!");
             return false;
-         }
-      }
-    }
-  ])
-  .then(managerInput => {
-   const { name, id, email, officeNumber } = managerInput;
-   const manager = new Manager(name, id, email, officeNumber);
-   team.push(manager);
-   console.log(manager); 
-  })
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the manager's Email?",
+        validate: (email) => {
+          //Regex email
+          valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+          if (valid) {
+            return true;
+          } else {
+            console.log("Please enter manager's Email!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+        validate: (officeInput) => {
+          if (officeInput) {
+            return true;
+          } else {
+            console.log("Please enter manager's office number!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((managerInput) => {
+      const { name, id, email, officeNumber } = managerInput;
+      const manager = new Manager(name, id, email, officeNumber);
+      team.push(manager);
+      console.log(manager);
+    });
 };
